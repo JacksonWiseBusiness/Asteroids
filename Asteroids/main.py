41be22eec8152f8fe8d4d1bec17efd6921597cff
jacksonwisebusiness <jacksonwise.business@gmail.com>
2026-09-22 13:15:03 -0400

    D: clock

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -8,7 +8,9 @@ def main():
     #print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt: float = 0.0
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +18,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
         
         
 if __name__ == "__main__":
