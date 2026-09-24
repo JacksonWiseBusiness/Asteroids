23c8de2c64d2142870f42d6c678f81deff8064d7
jacksonwisebusiness <jacksonwise.business@gmail.com>
2026-09-24 01:25:24 -0400

    Added player movement

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -20,8 +20,11 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
         
         
         
